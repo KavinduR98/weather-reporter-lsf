778f99b3eaf9a321d3985f177f6f7ce1497e2dde
@kavinduushan07 <kavinduushan07@gmail.com>
2025-06-13 19:00:43 +0530

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import dotenv from 'dotenv'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +9,8 @@ export default defineConfig({
     react(),
     tailwindcss()
   ],
+  define: {
+    'process.env.VITE_WEATHER_API_KEY': JSON.stringify(process.env.VITE_WEATHER_API_KEY),
+    'process.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL)
+  }
 })
